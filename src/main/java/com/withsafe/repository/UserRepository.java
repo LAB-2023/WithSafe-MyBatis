c3fda0aa3b744d6cf61d75ec5f4558cf84df64e6
LeeJungKyun <naeric7@naver.com>
2023-09-21 17:03:22 +0900

    UserRepository 추가

--- src/main/java/com/withsafe/repository/UserRepository.java
+++ src/main/java/com/withsafe/repository/UserRepository.java
@@ -1,4 +1,30 @@
 package com.withsafe.repository;
 
+import com.withsafe.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
 public class UserRepository {
+    private final EntityManager em;
+
+    public void save(User user){
+        em.persist(user);
+    }
+    public User findOne(Long id){
+        return em.find(User.class, id);
+    }
+    public List<User> findAll(){
+        return em.createQuery("select u from User u",User.class)
+                .getResultList();
+    }
+    public List<User> findByName(String name){
+        return em.createQuery("select u from User u where u.name = :name", User.class)
+                .setParameter("name",name)
+                .getResultList();
+    }
 }
