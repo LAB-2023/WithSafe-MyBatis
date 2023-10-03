073d03709d097bb11d3256bac478cd0547c9a542
yseo14 <lmslys14@naver.com>
2023-10-03 16:04:05 +0900

    [ADD] deviceSetting -> service, controller, repository 작성 (테스트 해봐야함)

---
+++ src/main/java/com/withsafe/domain/deviceSetting/application/DeviceSettingService.java
@@ -0,0 +1,42 @@
+package com.withsafe.domain.deviceSetting.application;
+
+import com.withsafe.domain.deviceSetting.dao.DeviceSettingRepository;
+import com.withsafe.domain.deviceSetting.domain.DeviceSetting;
+import com.withsafe.domain.deviceSetting.dto.DeviceSettingDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * 필요기능
+ * 1. 디바이스 세팅 저장
+ * 2. 기존 디바이스 세팅 조회(기존에 세팅값을 화면에 띄워줘야함)
+ *
+ * 데이터를 쌓을 것인지 하나의 데이터를 수정만 할 것인지..
+ */
+@Service
+@RequiredArgsConstructor
+public class DeviceSettingService {
+    private final DeviceSettingRepository deviceSettingRepository;
+
+
+    /**
+     * 기존 디바이스 세팅 조회
+     */
+    @Transactional
+    public DeviceSettingDTO viewDeviceSetting() {
+        DeviceSetting settingData = deviceSettingRepository.findTopByOrderByIdDesc();
+        return settingData.toDeviceSettingDTO();
+    }
+
+    /**
+     * 디바이스 세팅 저장
+     */
+    @Transactional
+    public Long saveDeviceSetting(DeviceSettingDTO saveRequest) {
+        DeviceSetting deviceSetting = saveRequest.toEntity();
+        deviceSettingRepository.save(deviceSetting);
+        return deviceSetting.getId();
+    }
+
+}
