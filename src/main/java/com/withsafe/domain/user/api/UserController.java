303c8cef4b9096a25b1554cf8a23adff12d8bfc0
Lee <lych0918@gmail.com>
2023-12-06 20:46:17 +0900

    [Refactor] 워치 Repository 부서 포함 등록 추가

--- src/main/java/com/withsafe/domain/user/api/UserController.java
+++ src/main/java/com/withsafe/domain/user/api/UserController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @CrossOrigin("http://localhost:3000")
-@RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
