0e4c09eda8583476ab72a24bb8fcca95137fec38
yseo14 <lmslys14@naver.com>
2023-12-09 17:40:32 +0900

    Refactor: 환경센서 디바이스 등록, 부서로 검색/ 시리얼넘버로 검색 구현

--- src/main/java/com/withsafe/domain/env/dao/EnvSensorDataRepository.java
+++ src/main/java/com/withsafe/domain/env/dao/EnvSensorDataRepository.java
@@ -1,11 +1,52 @@
 package com.withsafe.domain.env.dao;
 
+import com.withsafe.domain.env.domain.EnvSensor;
 import com.withsafe.domain.env.domain.EnvSensorData;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface EnvSensorDataRepository extends JpaRepository<EnvSensorData, Long> {
 
     EnvSensorData findTopByOrderByCreatedDateDesc();
+
+//    @Query("SELECT esd\n" +
+//            "FROM Department d\n" +
+//            "JOIN d.outdoorMapList om\n" +
+//            "JOIN om.envSensorList es\n" +
+//            "JOIN es.envSensorDataList esd\n" +
+//            "WHERE d.name = :deptName\n" +
+//            "AND esd.id IN (\n" +
+//            "    SELECT MAX(esdInner.createdDate)\n" +
+//            "    FROM Department dInner\n" +
+//            "    JOIN dInner.outdoorMapList omInner\n" +
+//            "    JOIN omInner.envSensorList esInner\n" +
+//            "    JOIN esInner.envSensorDataList esdInner\n" +
+//            "    WHERE dInner.name = :deptName\n" +
+//            "    GROUP BY esInner.id\n" +
+//            ")")
+    @Query("SELECT esd\n" +
+            "FROM Department d\n" +
+            "JOIN d.outdoorMapList om\n" +
+            "JOIN om.envSensorList es\n" +
+            "JOIN es.envSensorDataList esd\n" +
+            "WHERE d.name = :deptName\n" +
+            "AND esd.createdDate = (\n" +
+            "    SELECT MAX(esdInner.createdDate)\n" +
+            "    FROM Department dInner\n" +
+            "    JOIN dInner.outdoorMapList omInner\n" +
+            "    JOIN omInner.envSensorList esInner\n" +
+            "    JOIN esInner.envSensorDataList esdInner\n" +
+            "    WHERE dInner = d\n" +
+            "    AND esInner = es\n" +
+            ")")
+    List<EnvSensorData> findEnvSensorAndDataByDept(
+            @Param("deptName") String deptName);
+
+    List<EnvSensorData> findAllById(Long id);
+
 }
--- src/main/java/com/withsafe/domain/env/dao/EnvSensorRepository.java
+++ src/main/java/com/withsafe/domain/env/dao/EnvSensorRepository.java
@@ -14,7 +14,25 @@ public interface EnvSensorRepository extends JpaRepository<EnvSensor, Long> {
     @Query("SELECT es FROM EnvSensor es JOIN es.outdoorMap om WHERE om.department.name = :dept_name")
     List<EnvSensor> findEnvSensorByDept(
             @Param("dept_name") String deptName);
+//    @Query("SELECT esd\n" +
+//            "FROM Department d\n" +
+//            "JOIN d.outdoorMapList om\n" +
+//            "JOIN om.envSensorList es\n" +
+//            "JOIN es.envSensorDataList esd\n" +
+//            "WHERE d.name = :dept_name\n" +
+//            "AND esd.id IN (\n" +
+//            "    SELECT MAX(esdInner.id)\n" +
+//            "    FROM Department dInner\n" +
+//            "    JOIN dInner.outdoorMapList omInner\n" +
+//            "    JOIN omInner.envSensorList esInner\n" +
+//            "    JOIN esInner.envSensorDataList esdInner\n" +
+//            "    WHERE dInner.name = :deptName\n" +
+//            "    GROUP BY esInner.id\n" +
+//            ")")
+//    List<EnvSensor> findEnvSensorByDept(
+//            @Param("dept_name") String deptName);
 
-    List<EnvSensor> findBySerialNum(String serialNum);
+
+    EnvSensor findBySerialNum(String serialNum);
 
 }
