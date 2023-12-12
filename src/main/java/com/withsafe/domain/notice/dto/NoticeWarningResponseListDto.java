f662755a020ad6ae044fac6de846fc05bf7fa4dc
Lee <lych0918@gmail.com>
2023-12-12 13:43:34 +0900

    [Refactor] 워치 api 로그인 해제, Notice 조치 개수, 미조치 개수 파악 & 리턴

---
+++ src/main/java/com/withsafe/domain/notice/dto/NoticeWarningResponseListDto.java
@@ -0,0 +1,23 @@
+package com.withsafe.domain.notice.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
+
+@Getter
+@NoArgsConstructor
+public class NoticeWarningResponseListDto {
+    private Page<NoticeWarningResponseDto> list;
+    private long entire;
+    private long solve;
+    private long notSolve;
+
+    @Builder
+    public NoticeWarningResponseListDto(Page<NoticeWarningResponseDto> list, long entire, long solve, long notSolve) {
+        this.list = list;
+        this.entire = entire;
+        this.solve = solve;
+        this.notSolve = notSolve;
+    }
+}
